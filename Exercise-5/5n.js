003d4cd6dde9b4bc85b39a5ca20afe8d577a3995
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 15:29:17 +0530

    Create 5n.js

---
+++ Exercise-5/5n.js
@@ -0,0 +1,18 @@
+<DOCTYPE.html>
+    <html>
+      <head>
+      <title>Calculator</title>
+        <style>
+
+
+        </style>
+      </head>
+      <body>
+      
+        <script>
+             let calculation = '';
+        </script>
+
+
+      </body>
+    </html>
