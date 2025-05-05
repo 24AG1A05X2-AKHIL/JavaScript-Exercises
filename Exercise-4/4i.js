15522498db1909d6fe53ea839abc4b206d974efe
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 10:12:04 +0530

    Create 4i.js

---
+++ Exercise-4/4i.js
@@ -0,0 +1,22 @@
+<DOCTYPE.html>
+<html>
+  <head>
+    
+    <style>
+      .add{
+        background-color: yellow;
+      }
+      .Buy{
+        background-color: orange;
+      }
+
+    </style>
+  </head>
+  <body>
+    <p>Adults Plain Cotton T-Shirts</p>
+    <p>Price: $7.99</p>
+    <button class="add">Add to cart</button>
+    <button class="Buy">Buy now</button>
+    
+  </body>
+</html>
