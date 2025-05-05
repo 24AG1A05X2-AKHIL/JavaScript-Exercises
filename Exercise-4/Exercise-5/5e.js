f02525b23f3df02bff15228632143dd22157a0d4
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 11:41:13 +0530

    Create 5e.js

---
+++ Exercise-4/Exercise-5/5e.js
@@ -0,0 +1,19 @@
+<DOCTYPE.html>
+<html>
+  <head>
+    <title>Variables</title>
+    <style>
+
+
+    </style>
+  </head>
+  <body>
+    <script>
+     let cost = 3 + (2*3) + 9;
+     let result = cost * 0.1;
+     
+    </script>
+ 
+
+  </body>
+</html>
