0895892cb65e3244c282940372dea07dfcc408c2
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 15:59:37 +0530

    Create 5o.js

---
+++ Exercise-5/5o.js
@@ -0,0 +1,25 @@
+<DOCTYPE.html>
+    <html>
+      <head>
+      <title>Calculator</title>
+        <style>
+
+
+        </style>
+      </head>
+      <body>
+        
+      <button onclick="
+        calculation += '1';
+        console.log(calculation);
+      ">1</button>
+      
+     
+        
+        <script>
+           let calculation = '';
+        </script>
+
+
+      </body>
+    </html>
