f9923bb0691e2a68ccde01f92ebdcc6251dffa61
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 11:37:19 +0530

    Create 5c.js

---
+++ Exercise-4/Exercise-5/5c.js
@@ -0,0 +1,18 @@
+<DOCTYPE.html>
+<html>
+  <head>
+    <title>Variables</title>
+    <style>
+
+
+    </style>
+  </head>
+  <body>
+    <script>
+     let cost = 3 + (2*3) + 9;
+     
+    </script>
+ 
+
+  </body>
+</html>
