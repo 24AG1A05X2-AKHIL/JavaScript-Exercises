1c2c1db013c1f860c9174becab8c15e6b43d0beb
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 10:21:37 +0530

    Create 4j.js

---
+++ Exercise-4/4j.js
@@ -0,0 +1,25 @@
+<DOCTYPE.html>
+<html>
+  <head>
+    
+    <style>
+      .add{
+        background-color: yellow;
+      }
+      .Buy{
+        background-color: orange;
+      }
+
+    </style>
+  </head>
+  <body>
+    <p>Adults Plain Cotton T-Shirts</p>
+    <p>Price: $7.99</p>
+    <button class="add" onclick="
+    alert(`Added`)">Add to cart</button>
+    <button class="Buy" onclick="alert('Purchased');console.log(`Loading...`);">
+      Buy now
+    </button>
+    
+  </body>
+</html>
