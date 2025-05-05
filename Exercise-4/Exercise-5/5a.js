3ad1329e8b333718f5f6cfd0e67a02671dcbe717
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 11:32:13 +0530

    Create 5a.js

---
+++ Exercise-4/Exercise-5/5a.js
@@ -0,0 +1,17 @@
+<DOCTYPE.html>
+<html>
+  <head>
+    <title>Variables</title>
+    <style>
+
+
+    </style>
+  </head>
+  <body>
+    <script>
+      let variable = 'Akhil';
+    </script>
+ 
+
+  </body>
+</html>
