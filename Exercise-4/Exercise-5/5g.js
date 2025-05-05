f50ff7ab1bb12fc805332186e3c7312648c0ae4d
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 11:45:25 +0530

    Create 5g.js

---
+++ Exercise-4/Exercise-5/5g.js
@@ -0,0 +1,21 @@
+<DOCTYPE.html>
+<html>
+  <head>
+    <title>Variables</title>
+    <style>
+
+
+    </style>
+  </head>
+  <body>
+    <script>
+     let cost = 3 + (2*3) + 9;
+     let tax = cost * 0.1;
+     let totalCost = cost + tax;
+     
+     console.log(`Total cost: $${totalCost}`);
+    </script>
+ 
+
+  </body>
+</html>
