837e4ed294ce63cfb5e73262e89649ade0b55232
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 11:34:15 +0530

    Create 5b.js

---
+++ Exercise-4/Exercise-5/5b.js
@@ -0,0 +1,18 @@
+<DOCTYPE.html>
+<html>
+  <head>
+    <title>Variables</title>
+    <style>
+
+
+    </style>
+  </head>
+  <body>
+    <script>
+      let variable = 'Akhil';
+      console.log(`My name is: ${variable}`);
+    </script>
+ 
+
+  </body>
+</html>
