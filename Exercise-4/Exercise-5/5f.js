540e29dbd6a14f2b23268b1820cbbbb1510aeb05
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-05 11:43:39 +0530

    Create 5f.js

---
+++ Exercise-4/Exercise-5/5f.js
@@ -0,0 +1,20 @@
+<DOCTYPE.html>
+<html>
+  <head>
+    <title>Variables</title>
+    <style>
+
+
+    </style>
+  </head>
+  <body>
+    <script>
+     let cost = 3 + (2*3) + 9;
+     let tax = cost * 0.1;
+     
+     console.log(`Tax(10%): $${tax}`);
+    </script>
+ 
+
+  </body>
+</html>
