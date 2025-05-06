aa8b5c42e105715e785fe9d84fc6348f1295dd05
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-06 16:09:54 +0530

    Create 6d.js

---
+++ Exercise-6/6d.js
@@ -0,0 +1,19 @@
+<DOCTYPE.html>
+  <html>
+    <head>
+      <title>Exercise-6</title>
+    </head>
+    <body>
+      <script>
+        let age = 11;
+        
+        if(age < 6 || age === 6) {
+          console.log('Discount');
+        }
+        else  {
+          console.log('No discount');
+        }
+       
+      </script>
+    </body>
+  </html>
