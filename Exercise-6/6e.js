792eb98a03c499724026d387ed25614b19ec4092
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-06 16:20:18 +0530

    Create 6e.js

---
+++ Exercise-6/6e.js
@@ -0,0 +1,19 @@
+<DOCTYPE.html>
+  <html>
+    <head>
+      <title>Exercise-6</title>
+    </head>
+    <body>
+      <script>
+        let age = 3;
+        const isHoliday = true;
+        if((age < 6 || age === 6) && (isHoliday === false)) {
+          console.log('Discount');
+        }
+        else  {
+          console.log('No discount');
+        }
+       
+      </script>
+    </body>
+  </html>
