a613450a38e2b5b8474a617eeb02d12a2ba8002d
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-06 15:55:25 +0530

    Create 6a.js

---
+++ Exercise-6/6a.js
@@ -0,0 +1,20 @@
+<DOCTYPE.html>
+  <html>
+    <head>
+      <title>Exercise-6</title>
+    </head>
+    <body>
+      <script>
+        let hour = 15
+        if(hour>6 && hour<12) {
+          console.log('Good morning!');
+        }
+        else if(hour>13 && hour<17) {
+          console.log('Good afternoon!');
+        }
+        else {
+          console.log('Good night');
+        }
+      </script>
+    </body>
+  </html>
