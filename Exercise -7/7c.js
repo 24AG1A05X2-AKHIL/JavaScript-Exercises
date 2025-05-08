11170de9d5e8a32363e88999f95cf065ff8131e0
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-08 11:58:33 +0530

    Create 7c.js

---
+++ Exercise -7/7c.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exercise-7</title>
+  </head>
+  <body>
+
+
+    <script>
+      function greet(name){
+        if (!name){
+          console.log('Hi there!');}
+        else {
+          console.log(`Hello ${name}`);
+        }
+      }
+        
+      greet();
+      greet('akhil');
+      greet('ahsan');
+
+    </script>
+  </body>
+</html>
