db87e8468190b577b397b64d5169f9e4b4b5207d
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-08 12:09:12 +0530

    Create 7e.js

---
+++ Exercise -7/7e.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exercise-7</title>
+  </head>
+  <body>
+
+
+    <script>
+      function convertToCelcius(Fahrenheit){
+        let Celcius = ((Fahrenheit - 32) * (5/9));
+        return Celcius;
+      }
+      console.log(convertToCelcius(86));
+    </script>
+  </body>
+</html>
