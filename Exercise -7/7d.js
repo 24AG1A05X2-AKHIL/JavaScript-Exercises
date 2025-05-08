f3a590a907562fde371b70c9de7ccc3d5887553c
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-08 12:04:34 +0530

    Create 7d.js

---
+++ Exercise -7/7d.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exercise-7</title>
+  </head>
+  <body>
+
+
+    <script>
+      function convertToFahrenheit(celcius){
+        let Fahrenheit = (celcius * (9/5)) + 32;
+        return Fahrenheit;
+      }
+      console.log(convertToFahrenheit(25));
+    </script>
+  </body>
+</html>
