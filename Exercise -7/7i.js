415df58b5bb2a95bae7739cdebcd84a5c13f79fc
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-09 12:56:13 +0530

    Create 7i.js

---
+++ Exercise -7/7i.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exercise-7</title>
+  </head>
+  <body>
+
+
+    <script>
+      let length = '';
+      let from = '';
+      let to = '';
+      function convertLength(length, from, to){
+        if (from == 'miles' && to == 'km'){
+          to = length * 1.6;
+          console.log(`${to} km`);
+        } else if(from == 'km' && to == 'miles'){
+          from = length / 1.6;
+          console.log(`${from} miles`);
+        }else if (from == 'miles' && to == 'ft'){
+          to = length * 5280;
+          console.log(`${to} ft`);
+
+        } else if (from == 'km' && to == 'ft'){
+          to = length * 3281;
+          console.log(`${to} ft`)
+        }else {
+          unit = from;
+          console.log(`Invalid unit: ${unit}`)
+        }
+      }
+      convertLength(5, 'miles', 'km');
+      convertLength(5, 'miles', 'ft');
+      convertLength(5, 'km', 'ft');
+      convertLength(5, 'lbs', 'lbs');
+      
+    </script>
+  </body>
+</html>
