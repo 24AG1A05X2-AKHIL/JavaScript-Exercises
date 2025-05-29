fa21adb1c7cbed5266662988b576a7a8a2f62d1c
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-29 10:28:22 +0530

    Create 8b.js

---
+++ Exercise-8/8b.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Exercise-8</title>
+  </head>
+  <body>
+    <script>
+     const amazonProject = {
+      product: 'basketball',
+      price:    2095
+     };
+     console.log(amazonProject.product);
+     amazonProject.price = 2595;
+     console.log(amazonProject.price);
+    </script>
+  </body>
+</html>
