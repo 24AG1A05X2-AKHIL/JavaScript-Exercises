ad142d39e7fd0232ab91e81faebab9aa227df734
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-30 14:06:40 +0530

    Create 8h.js

---
+++ Exercise-8/8h.js
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <p>
+      <button onclick="
+      updateCalculation('1')
+      print()
+      ">1</button>
+
+      <button onclick="
+        updateCalculation('2')
+        print()
+      ">2</button>
+
+      <button onclick="
+        updateCalculation('3')
+        print()
+      ">3</button>
+
+      <button onclick="
+        updateCalculation(' + ')
+        print()
+      ">+</button>
+    </p>
+
+    <p>
+      <button onclick="
+        updateCalculation('4')
+        print()
+      ">4</button>
+
+      <button onclick="
+        updateCalculation('5')
+        print()
+      ">5</button>
+
+      <button onclick="
+        updateCalculation('6')
+        print()
+      ">6</button>
+
+      <button onclick="
+        updateCalculation(' - ')
+        print()
+      ">-</button>
+    </p>
+
+    <p>
+      <button onclick="
+        updateCalculation('7')
+        print()
+      ">7</button>
+
+      <button onclick="
+        updateCalculation('8')
+        print()
+      ">8</button>
+
+      <button onclick="
+        updateCalculation('9')
+        print()
+      ">9</button>
+
+      <button onclick="
+        updateCalculation(' * ')
+        print()
+      ">*</button>
+    </p>
+
+    <p>
+      <button onclick="
+        updateCalculation('0')
+        print()
+      ">0</button>
+
+      <button onclick="
+        updateCalculation('.')
+        print()
+      ">.</button>
+
+      <button onclick="
+        output()
+      ">=</button>
+
+      <button onclick="
+        updateCalculation(' / ')
+        print()
+      ">/</button>
+    </p>
+
+    <p>
+      <button onclick="
+        calculation = '';
+        localStorage.setItem(`calculation`,JSON.stringify(calculation))
+        console.log('Cleared.');
+      ">Clear</button>
+    </p>
+
+    <script>
+      
+      let calculation = ''||JSON.parse(localStorage.getItem("calculation"))
+      function updateCalculation(ak){
+        calculation+=ak;
+        localStorage.setItem("calculation",JSON.stringify(calculation))
+      }
+      function print(){
+        console.log(calculation);
+      }
+      function output(){
+        calculation1=eval(calculation);
+        console.log(calculation1);
+        localStorage.setItem("calculation",JSON.stringify(calculation))
+      }
+      
+    </script>
+  </body>
+</html>
