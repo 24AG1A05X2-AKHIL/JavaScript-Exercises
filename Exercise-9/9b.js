c2d2bf0297d4497bb3071a0b0f50a36f82632009
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-05-31 15:31:08 +0530

    Create 9b.js

---
+++ Exercise-9/9b.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exercise-9</title>
+  </head>
+  <body>
+  <button>9a</button>
+  <button class="hello">9b</button>
+
+    <script>
+      console.log(document.querySelector('button'));
+      document.querySelector('.hello') .innerHTML = '9b done!';
+      
+    </script>
+  </body>
+</html>
