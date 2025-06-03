5070501fcaae12e95454760b91d1aec4a0a6fafd
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-06-03 11:49:16 +0530

    Create 10b.js

---
+++ Exercise-10/10b.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+  <title>Exercise-10</title>
+</head>
+<style>
+
+  .Uber{
+    background-color: black;
+    color: white;
+    height: 32px;
+    width: 110px;
+    border-radius: 6px;
+    border: none;
+  }
+  .UberButton{
+    font-weight: bold;
+    height: 32px;
+    width: 140px;
+    background-color: rgb(230, 230, 230);
+    border-radius: 6px;
+    border: none;
+
+  }
+</style>
+<body>
+  
+  <button class="Uber">Request now</button>
+  <button class="UberButton">Schedule for later </button>
+
+  <script>
+   
+</script>
+<script>
+</script>
+</body>
+
+</html>
