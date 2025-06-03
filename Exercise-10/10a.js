28faa184de726b61278014848e67cf2369fab054
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-06-03 11:47:32 +0530

    Create 10a.js

---
+++ Exercise-10/10a.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+  <title>Exercise-10</title>
+</head>
+<style>
+  .Amazon{
+    height: 32px;
+    width: 150px;
+    border-radius:  16px;
+    background-color: rgb(255, 216, 20);
+    border: none;
+    font-weight: bold;
+  }
+
+</style>
+<body>
+  <button class="Amazon">Add to Cart</button>
+ 
+
+  <script>
+   
+</script>
+<script>
+</script>
+</body>
+
+</html>
