e0f214055738e16ceb0247479d19f66d0de8df97
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-06-04 10:37:53 +0530

    Create 11b.js

---
+++ Exercise-11/11b.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Exercise-11</title>
+
+ 
+</head>
+
+<body>
+ 
+
+    <script>
+     function getLastValue(array){
+       for(let i = 0; i < array.length; i++){
+        let lastValue = array[array.length-1];
+        console.log(lastValue);
+
+       }
+     }
+     let Array = [1, 20, 22, 24, 5];
+     getLastValue(Array);
+    </script>
+  </body>
+</html>
