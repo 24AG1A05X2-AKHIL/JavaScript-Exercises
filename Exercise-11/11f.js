6edf568c67151aaf261c8ad2023cc96b0c799550
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-06-04 11:58:30 +0530

    Create 11f.js

---
+++ Exercise-11/11f.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Exercise-11</title>
+
+ 
+</head>
+
+<body>
+ 
+
+    <script>
+      j = 0;
+      while(j <= 10){
+        console.log(j);
+        j++;
+      }
+    i = 5;
+    while(i >= 0){
+     console.log(i);
+     i--;
+    }
+      
+    
+    </script>
+  </body>
+</html>
