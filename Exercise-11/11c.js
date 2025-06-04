d1d7581eea6620b7454ff712e0fd56b578a3aa4b
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-06-04 11:01:24 +0530

    Update 11c.js

--- Exercise-11/11c.js
+++ Exercise-11/11c.js
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Exercise-11</title>
 
+ 
+</head>
+
+<body>
+ 
+
+    <script>
+    function arraySwap(array){
+
+      let array1 = array[array.length - array.length];
+      let array2 = array[array.length-1];
+      array[0] = array2;
+      array[array.length-1] = array1;
+      console.log(array);
+    
+    }
+    
+    let Array = [1, 20, 22, 24, 5];
+    arraySwap(Array);
+    </script>
+  </body>
+</html>
