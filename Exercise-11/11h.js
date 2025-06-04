12399249943e842e56a5b682c28f7122907092c0
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-06-04 14:01:01 +0530

    Create 11h.js

---
+++ Exercise-11/11h.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Exercise-11</title>
+
+ 
+</head>
+
+<body>
+ 
+
+    <script>
+      function addOne(array){
+     for(i=0;i<array.length;i++){
+      array[i]++;
+     }
+      }
+    
+     
+     let array = [1, 2, 3];
+     addOne(array);
+     console.log(array);
+    
+    </script>
+  </body>
+</html>
