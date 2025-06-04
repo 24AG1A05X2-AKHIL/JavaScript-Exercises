35455736c6c24d473c2cc5dd7347a477060362c8
24AG1A05X2-AKHIL <akhilbommu2006@gmail.com>
2025-06-04 11:53:49 +0530

    Create 11e.js

---
+++ Exercise-11/11e.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Exercise-11111</title>
+
+ 
+</head>
+
+<body>
+ 
+
+    <script>
+    for(let i = 5; i >= 0 ; i--){
+      console.log(i);
+    }
+    </script>
+  </body>
+</html>
